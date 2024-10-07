81d928bd47c67b0d5119e40840f8d74baa77ce5b
KevinNobre <kevinchristian.nobre@gmail.com>
2024-10-07 01:07:47 -0300

    Adicionando a entidade UsuarioPaciente e iniciando a criação dos Repositories

---
+++ SmartoothAI.Domain/Entities/UsuarioPaciente.cs
@@ -0,0 +1,60 @@
+﻿namespace SmartoothAI.Domain.Entities
+{
+    public class UsuarioPaciente
+    {
+        public int PacienteId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime? DataNasc { get; set; }
+
+        public string Genero { get; set; }
+
+        public string Cep { get; set; }
+
+        public string Logradouro { get; set; }
+
+        public string Numero { get; set; }
+
+        public string Complemento { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Uf { get; set; }
+
+        public string Contato { get; set; }
+
+        public decimal? Pontos { get; set; }
+
+        public decimal? Descontos { get; set; }
+
+        public UsuarioPaciente(int pacienteId, string nome, string sobrenome, string email,
+                               DateTime? dataNasc, string genero, string cep,
+                               string logradouro, string numero, string complemento,
+                               string bairro, string cidade, string uf,
+                               string contato, decimal? pontos, decimal? descontos)
+        {
+            PacienteId = pacienteId;
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Email = email;
+            DataNasc = dataNasc;
+            Genero = genero;
+            Cep = cep;
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+            Uf = uf;
+            Contato = contato;
+            Pontos = pontos;
+            Descontos = descontos;
+        }
+    }
---
+++ SmartoothAI.Domain/Repositories/IUsuarioPacienteRepository.cs
@@ -0,0 +1,17 @@
+﻿using SmartoothAI.Domain.Entities;
+
+namespace SmartoothAI.Domain.Repositories
+{
+    public interface IUsuarioPacienteRepository
+    {
+        Task<UsuarioPaciente> GetByIdAsync(int id);
+
+        Task<IEnumerable<UsuarioPaciente>> GetAllAsync();
+
+        Task AddAsync(UsuarioPaciente usuarioPaciente);
+
+        Task UpdateAsync(UsuarioPaciente usuarioPaciente);
+
+        Task DeleteAsync(int id);
+    }
+}
