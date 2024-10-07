2979cc1085d5072e8cce0ca4ca81ca8ab3161d3d
KevinNobre <kevinchristian.nobre@gmail.com>
2024-10-07 00:14:53 -0300

    Adicionando as entidades Atendimento e Dica

---
+++ SmartoothAI.Domain/Entities/Atendimento.cs
@@ -0,0 +1,41 @@
+﻿namespace SmartoothAI.Domain.Entities
+{
+    internal class Atendimento
+    {
+       public int AtendimentoId { get; set; }
+
+       public string Descricao { get; set; }
+
+       public DateTime? Data { get; set; }
+
+       public TimeSpan? Hora { get; set; }
+
+       public char? InclusoPlano { get; set; }
+
+       public decimal? Custo { get; set; }
+
+       public int ProfissionalId { get; set; }
+
+       public int UsuarioPacienteId { get; set; }
+
+       public int RecomendacaoTratId { get; set; }
+
+       public int ProfissionalId1 { get; set; }
+
+        public Atendimento(int atendimentoId, string descricao, DateTime? data, TimeSpan? hora,
+                           char? inclusoPlano, decimal? custo, int profissionalId,
+                           int usuarioPacienteId, int recomendacaoTratId, int profissionalId1)
+        {
+            AtendimentoId = atendimentoId;
+            Descricao = descricao;
+            Data = data;
+            Hora = hora;
+            InclusoPlano = inclusoPlano;
+            Custo = custo;
+            ProfissionalId = profissionalId;
+            UsuarioPacienteId = usuarioPacienteId;
+            RecomendacaoTratId = recomendacaoTratId;
+            ProfissionalId1 = profissionalId1;
+        }
+    }
+}
---
+++ SmartoothAI.Domain/Entities/Dica.cs
@@ -0,0 +1,21 @@
+﻿namespace SmartoothAI.Domain.Entities
+{
+    public class Dica
+    {
+        public int DicaId { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int ProntuarioProntuarioId { get; set; }
+
+        public int UsuarioPacientePacienteId { get; set; }
+
+        public Dica(int dicaId, string descricao, int prontuarioProntuarioId, int usuarioPacientePacienteId)
+        {
+            DicaId = dicaId;
+            Descricao = descricao;
+            ProntuarioProntuarioId = prontuarioProntuarioId;
+            UsuarioPacientePacienteId = usuarioPacientePacienteId;
+        }
+    }
+}
