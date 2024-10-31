b73be0a971f17b32c9d93f14d663ee9a5706d890
KevinNobre <kevinchristian.nobre@gmail.com>
2024-10-31 12:39:21 -0300

    Criando a classe de Configuração Web

---
+++ SmartoothAI/Configuration/WebConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace SmartoothAI.WebAPI.Configurations
+{
+    public static class WebConfiguration
+    {
+        public static void ConfigureWebApp(WebApplicationBuilder builder)
+        {// Configuração para MVC
+            builder.Services.AddHttpsRedirection(options => options.HttpsPort = 5001); 
+        }
+
+        public static void UseWebApp(WebApplication app)
+        {
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"); 
+        }
+    }
+}
