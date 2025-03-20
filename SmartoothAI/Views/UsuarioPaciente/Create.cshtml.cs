2f588623c5ec568b963db0d4b53b12bab5b3f39a
KevinNobre <kevinchristian.nobre@gmail.com>
2025-03-20 18:24:36 -0300

    feat: Views UsuarioPaciente

---
+++ SmartoothAI/Views/UsuarioPaciente/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SmartoothAI.WebAPI.Views.Paciente
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
